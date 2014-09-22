226a8e82939260fcd6cb25f02d18f457adaa70d6
ChristianLemieux <cjl5470@gmail.com>
2014-09-22 19:51:03 -0400

    Asteroid Prefab Null
    See Title

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Asteroid;
+	private Vector3 xyz;
+
+	// Use this for initialization
+	void Start () {
+		Asteroid = GameObject.Find("Asteroid");
+		xyz = new Vector3(30, Random.Range(0,30),0);
+		Instantiate(Asteroid, xyz, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
