dea2a2684679f2d47ed3492c31afff766795e50f
pco1853 <pco1853@rit.edu>
2014-10-15 14:51:39 -0400

    Added Main Menu and Instructions Screen

---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/InstructionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//attatched to main camera of instructions scene
+public class InstructionsMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+	
+	void OnGUI()
+	{
+		//display background texture for main menu
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height),backgroundTexture);
+		
+		//displays buttons for menu/detects if it is clicked
+		if (GUI.Button(new Rect(Screen.width * .45f, Screen.height * .8f, Screen.width *.1f, Screen.height * .1f), "Back"))
+		{
+			Application.LoadLevel(Application.loadedLevel - 1);
+		}
+	}
+}
---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//attatched to Main Camera of Main Menu Scene
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	void OnGUI()
+	{
+		//display background texture for main menu
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height),backgroundTexture);
+	
+		//Displays Play Button and transitions to game when clicked
+		if (GUI.Button(new Rect(Screen.width * .45f, Screen.height * .4f, Screen.width *.1f, Screen.height * .1f), "Play Game"))
+		{
+			Application.LoadLevel(Application.loadedLevel + 2);
+		}
+
+		//Displays instructions button and transitions when pressed
+		if (GUI.Button(new Rect(Screen.width * .45f, Screen.height * .6f, Screen.width *.1f, Screen.height * .1f), "Instructions"))
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+
+}
